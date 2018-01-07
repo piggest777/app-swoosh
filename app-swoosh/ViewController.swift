6af345ec2589201d8a4283c529c82eba3dad33a7
Piggest <piggest@mail.ru>
2018-01-07 22:29:28 +0300

    segue staff

--- app-swoosh/ViewController.swift
+++ app-swoosh/ViewController.swift
@@ -19,11 +19,9 @@ class ViewController: UIViewController {
 
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func unwinFromSkillVc(unvindSegue: UIStoryboardSegue) {
+        
     }
-
 
 }
 
