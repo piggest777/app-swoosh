2c3809b54723359187a05c2ebc75eeb5fd7bc852
Piggest <piggest@mail.ru>
2018-01-07 23:58:57 +0300

    value legue logic

--- app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/Controller/LeagueVC.swift
@@ -9,19 +9,40 @@
 import UIKit
 
 class LeagueVC: UIViewController {
-
-
+    
+    var player: Player!
+    
+    @IBOutlet weak var nextBtn: BorderButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        player = Player()
+        
 
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func onNextTapped(_ sender: Any) {
-  
                   performSegue(withIdentifier: "skillVCSegue", sender: self)
-    
     }
 
-  
+    @IBAction func onMansTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+        
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+       selectLeague(leagueType: "coed")
+    }
+   
+    
+    func selectLeague(leagueType: String) {
+        player.desiredLegue = leagueType
+         nextBtn.isEnabled = true
+        
+    }
 }
---
+++ app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  player.swift
+//  app-swoosh
+//
+//  Created by Admin on 07.01.18.
+//  Copyright © 2018 piggest. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLegue: String?
+    var selectefSkillLevel: String?
+}
